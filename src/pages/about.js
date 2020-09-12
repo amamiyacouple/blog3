76abb54b35f16c9b15173a93246b33d87d95d69b
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-12 17:40:59 +0900

    about

---
+++ src/pages/about.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import image from "../images/about.png";
+
+const Aboutpage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const author = data.site.siteMetadata.author.name;
+  return (
+    <div>
+      <Layout location={location} title={siteTitle} author={author}>
+        <SEO title="About" />
+        <h1>About</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Aboutpage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author {
+          name
+        }
+      }
+    }
+  }
+`;
