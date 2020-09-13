9e6531079f8a9933768e563f0a9d4167d73dea71
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-13 14:01:37 +0900

    tag

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(
     `
       {
-        posts: allMarkdownRemark(
+        allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
