3ea367883da3f4d2b8c43282d5ac7ffb06a83ecb
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-14 23:58:19 +0900

    share

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,6 +14,7 @@ import Share from "../components/share";
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
+  const siteUrl = data.site.siteMetadata.siteUrl;
   const { previous, next } = pageContext
   
   return (
@@ -54,6 +55,11 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
           style={{
             marginBottom: rhythm(1),
           }}
+        />
+        <Share
+          title={post.frontmatter.title}
+          url={`${siteUrl}${slug}`}
+          description={post.excerpt}
         />
         <footer>
           <Bio />
