edfcc0fcfccc6e7e602a53157107a8f0f2a55e62
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-17 23:10:48 +0900

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,43 +1,10 @@
-import React from "react";
-import { useStaticQuery, Link, graphql } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 import "../styles/style.scss"
 
 const NavBar = () => {
-  const {
-    site: {
-      siteMetadata: { title },
-    },
-  } = useStaticQuery (
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-    )
-
   return (
     <nav className="navbar">
-      <header id="luxbar" class="luxbar-fixed">
-        <input type="checkbox" class="luxbar-checkbox" id="luxbar-checkbox"/>
-        <div class="luxbar-menu luxbar-menu-right luxbar-menu-dark">
-            <ul class="luxbar-navigation">
-                <li class="luxbar-header">
-                    <a href="https://amamiyacouple.work/" class="luxbar-brand">{title}</a>
-                    <label class="luxbar-hamburger luxbar-hamburger-doublespin" 
-                    id="luxbar-hamburger" for="luxbar-checkbox"> <span></span> </label>
-                </li>
-                <li class="luxbar-item"><Link to="/">HOME</Link></li>
-                <li class="luxbar-item"><Link to="/about">ABOUT</Link></li>
-                <li class="luxbar-item"><Link to="/contact">お仕事依頼</Link></li>
-            </ul>
-        </div>
-      </header>
-    </nav>
-    /*
       <ul className="navbar__ul">
         <li className="navbar__li">
           <Link to="/">HOME</Link>
@@ -49,9 +16,8 @@ const NavBar = () => {
           <Link to="/contact">お仕事依頼</Link>
         </li>
       </ul>
-    */
+    </nav>
   );
 };
-
 
 export default NavBar;
