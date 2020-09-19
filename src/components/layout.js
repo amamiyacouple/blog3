eaf6bf6676548ed4e0a38cfd0340ccadefc9da88
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 19:37:06 +0900

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -56,7 +56,6 @@ const Layout = ({ location, title, children }) => {
       marginLeft: `auto`,
       marginRight: `auto`,
       maxWidth: rhythm(30),
-      padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
     }}
     >
       <Header/>
