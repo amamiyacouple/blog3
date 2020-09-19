dbe25deaf0f661325a361ea0f907c546b0d9f07a
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 19:32:13 +0900

    header

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 
 const Header = ({ siteTitle }) => (
     <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/">Amamiyablog</Navbar.Brand>
+        <Navbar.Brand as={Link} href="/">Amamiyablog</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
