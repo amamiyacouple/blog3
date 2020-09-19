b13bd4e0318326d3111a16c0988320b4c5758552
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 19:06:51 +0900

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,14 +51,7 @@ const Layout = ({ location, title, children }) => {
     )
   }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
+    <div>
       <Header/>
       <header>{header}</header>
       <main>{children}</main>
