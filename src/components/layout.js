852debb79340a43bc4fce1360713d869fb63c2df
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 13:55:13 +0900

    header

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 //Add by myself
-import NavBar from "../components/navbar";
+//import NavBar from "../components/navbar"
+import Header from "../components/header"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -57,7 +58,7 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
-      <NavBar/>
+      <Header/>
       <header>{header}</header>
       <main>{children}</main>
       <footer>
