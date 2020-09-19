e94800ab75c45fe04a5eac86d784fb19d825f4ec
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 16:24:21 +0900

    bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,37 @@
-import React from 'react'
-import { useStaticQuery, Link, graphql } from 'gatsby'
-import '../styles/style.scss'
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import { Container, Navbar, Nav } from 'react-bootstrap' // ←追記
 
-export default function Header () {
-  const {
-    site: {
-      siteMetadata: { title },
-    },
-  } = useStaticQuery (
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
+const Header = ({ siteTitle }) => (
+// ↓ここから
+  <header className="bg-dark">
+    <Container>
+      <Navbar expand="md" variant="dark">
+        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse id="navbarResponsive">
+          <Nav as="ul" className="ml-auto">
+            <Nav.Item as="li">
+              <Link to="/page-2" className="nav-link" activeClassName="active">Page2</Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Link to="/page-3" className="nav-link" activeClassName="active">Page3</Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container> 
+  </header>
+// ↑ここまで
+)
 
-  return (
-    <header id="luxbar" class="luxbar-fixed">
-        <input type="checkbox" class="luxbar-checkbox" id="luxbar-checkbox"/>
-        <div class="luxbar-menu luxbar-menu-right luxbar-menu-dark">
-            <ul class="luxbar-navigation">
-                <li class="luxbar-header">
-                    <a href="#" class="luxbar-brand">LUXBAR</a>
-                    <label class="luxbar-hamburger luxbar-hamburger-doublespin" 
-                    id="luxbar-hamburger" for="luxbar-checkbox"> <span></span> </label>
-                </li>
-                <li class="luxbar-item"><a href="#">Item 1</a></li>
-                <li class="luxbar-item"><a href="#">Item 2</a></li>
-                <li class="luxbar-item"><a href="#">Item 3</a></li>
-                <li class="luxbar-item"><a href="#">Item 4</a></li>
-            </ul>
-        </div>
-    </header>
-  )
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 }
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
