1a0191e51c6a24b6120d0fff57bf5d553756a761
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 18:54:40 +0900

    header

--- src/components/header.js
+++ src/components/header.js
@@ -2,28 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 // add by myself
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 //import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
 const Header = ({ siteTitle }) => (
-    <header className="bg-dark">
-        <Container>
-            <Navbar expand="md" variant="dark">
-                <Navbar.Brand href="/">バリキャリ夫婦雨宮のブログ</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarResponsive" />
-                <Navbar.Collapse id="navbarResponsive">
-                    <Nav as="ul" className="ml-auto">
-                    <Nav.Item as="li">
-                        <Link to="/about" className="nav-link" activeClassName="active">プロフィール</Link>
-                    </Nav.Item>
-                    <Nav.Item as="li">
-                        <Link to="/contact" className="nav-link" activeClassName="active">お仕事依頼</Link>
-                    </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
-    </header>
+    <Navbar bg="dark" variant="dark" expand="true">
+        <Navbar.Brand href="/">バリキャリ夫婦雨宮のブログ</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse id="navbarResponsive">
+        <Nav className="mr-auto">
+            <Nav.Link href="/about">プロフィール</Nav.Link>
+            <Nav.Item as="li">
+                <Link to="/contact" className="nav-link" activeClassName="active">お仕事依頼</Link>
+            </Nav.Item>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 )
 
 Header.propTypes = {
