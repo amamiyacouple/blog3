bd272b22daa52699f6c109dd4b3a994f8e5666bc
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-09-19 17:35:04 +0900

    bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,27 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Container, Navbar, Nav } from 'react-bootstrap' // ←追記
+// add by myself
+//import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
 const Header = ({ siteTitle }) => (
-// ↓ここから
   <header className="bg-dark">
-    <Container>
-      <Navbar expand="md" variant="dark">
-        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarResponsive" />
-        <Navbar.Collapse id="navbarResponsive">
-          <Nav as="ul" className="ml-auto">
-            <Nav.Item as="li">
-              <Link to="/page-2" className="nav-link" activeClassName="active">Page2</Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Link to="/page-3" className="nav-link" activeClassName="active">Page3</Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container> 
+    <Navbar expand="md" variant="white">
+    <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarResponsive" />
+    <Navbar.Collapse id="navbarResponsive">
+        <Nav as="ul" className="ml-auto">
+        <Nav.Item as="li">
+            <Link to="/page-2" className="nav-link" activeClassName="active">Page2</Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+            <Link to="/page-3" className="nav-link" activeClassName="active">Page3</Link>
+        </Nav.Item>
+        </Nav>
+    </Navbar.Collapse>
+    </Navbar>
   </header>
-// ↑ここまで
 )
 
 Header.propTypes = {
