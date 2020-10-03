81cab815458e02e0de48fd3bc1c8677a4f8b6097
tomtomlife <tomohiro.sato.riko@gmail.com>
2020-10-03 20:18:59 +0900

    header

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,20 @@ import PropTypes from "prop-types"
 import React from "react"
 // add by myself
 import { Navbar, Nav } from 'react-bootstrap'
+import coupleImage from "../../content/assets/profile-pic.jpg"
 
 const Header = ({}) => (
     <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand as={Link} href="/">パワーカップルの生き方</Navbar.Brand>
+        <Navbar.Brand as={Link} href="/">
+          <img
+            alt=""
+            src={coupleImage}
+            width="30"
+            height="30"
+            className="d-inline-blovk align-top"
+          />
+          パワーカップルの生き方
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
